30a4a226e0509b6a6136d4fdd30bc300f4c998f3
Sivadass N <Sivadass N>
2017-05-24 19:14:52 +0530

    Adding initial configuration files

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="container">
+        
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
