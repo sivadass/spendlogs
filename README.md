e648bd9ce7b68c2ac49dcb230dc38ec3b670c4e4
Sivadass N <Sivadass N>
2017-05-24 19:35:25 +0530

    Added react parent component.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# react-expense-manager
+# React Expense Manager
+
+React and Redux powered app to manage your expenses on the go.
--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ class App extends React.Component{
   render(){
     return(
       <div className="container">
-        
+        <h1>Hola</h1>
       </div>
     )
   }
