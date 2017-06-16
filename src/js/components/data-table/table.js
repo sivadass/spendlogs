db5a6dbcfc381fff3f250b5654dab878200e8852
Sivadass N <nsivadass@gmail.com>
2017-06-16 09:30:35 +0530

    Added category icons

--- src/js/components/data-table/table.js
+++ src/js/components/data-table/table.js
@@ -4,7 +4,8 @@ const Table = (props) => (
 <table className="table">
  <thead>
   <tr>
-     <th colSpan="2" className="data-payee">Payee</th>
+     <th className="data-category">&nbsp;</th>
+     <th className="data-payee">Payee</th>
      <th className="data-payee">Date</th>
      <th className="data-comment">Comment</th>
      <th className="data-amount">Amount</th>
@@ -12,40 +13,40 @@ const Table = (props) => (
  </thead>
  <tbody>
    <tr>
-     <td className="data-category">Electricity</td>
+     <td className="data-category"><i className="material-icons data-category-icon">lightbulb_outline</i></td>
      <td className="data-payee">TNEB</td>
      <td className="data-date">10 AM, Wed, 05/12/17</td>
      <td className="data-comment">Check reading next month</td>
-    <td className="data-amount">1350</td>
+    <td className="data-amount currency-symbol">1350</td>
   </tr>
   <tr>
-     <td className="data-category">Internet</td>
+     <td className="data-category"><i className="material-icons data-category-icon">wifi</i></td>
      <td className="data-payee">Airtel Broadband</td>
      <td className="data-date">10.15 AM, Wed, 05/12/17</td>
      <td className="data-comment">Reduce usage of data.</td>
-    <td className="data-amount">999</td>
+    <td className="data-amount currency-symbol">999</td>
   </tr>
   <tr>
-     <td className="data-category">Clothes</td>
+     <td className="data-category"><i className="material-icons data-category-icon">local_offer</i></td>
      <td className="data-payee">Otto</td>
      <td className="data-date">8 PM, Tue, 04/12/17</td>
      <td className="data-comment">Two Shirts and one Pant.</td>
-    <td className="data-amount">1350</td>
+    <td className="data-amount currency-symbol">1350</td>
   </tr>
 
   <tr>
-     <td className="data-category">Books</td>
+     <td className="data-category"><i className="material-icons data-category-icon">book</i></td>
      <td className="data-payee">Flipkart</td>
      <td className="data-date">10 AM, Mon, 03/12/17</td>
      <td className="data-comment">Deepwork and ReWork</td>
-    <td className="data-amount">1350</td>
+    <td className="data-amount currency-symbol">1350</td>
   </tr>
   <tr>
-     <td className="data-category">Food</td>
+     <td className="data-category"><i className="material-icons data-category-icon">restaurant_menu</i></td>
      <td className="data-payee">Aasife and Brothers</td>
      <td className="data-date">10 PM, Sunday, 30/11/17</td>
      <td className="data-comment">3 Mutton Briyani</td>
-    <td className="data-amount">675</td>
+    <td className="data-amount currency-symbol">675</td>
   </tr>
  </tbody>
 </table>
