bccbfa9b11fec6d60d05084ad0e824829b86b0ce
Sivadass N <nsivadass@gmail.com>
2017-06-17 21:34:29 +0530

    Added table filters

--- src/js/components/data-table/data-filter.js
+++ src/js/components/data-table/data-filter.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const DataTable = (props) => (
-  <ul>
-    <li>Today</li>
-    <li>This Week</li>
-    <li>This Month</li>
-  </ul>
+  <div>
+    <ul>
+      <li><a href="#">Today</a></li>
+      <li><a href="#">This Week</a></li>
+      <li><a href="#">This Month</a></li>
+    </ul>
+    <div className="custom-duration">
+      <div className="form-group">
+        <input className="form-control" type="text" id="from-date" placeholder="From Date"/>
+        <label className="form-label" htmlFor="from-date">From Date <i className="material-icons">event</i></label>
+      </div>
+      <div className="form-group">
+        <input className="form-control" type="text" id="to-date" placeholder="To Date"/>
+        <label className="form-label" htmlFor="to-date">To Date <i className="material-icons">event</i></label>
+      </div>
+
+    </div>
+  </div>
 )
 
 export default DataTable;
