a990e2bb4b24551c03d5cbf42b240459ea6b3b09
Sivadass N <nsivadass@gmail.com>
2018-01-01 13:04:51 +0530

    Render multiple elements without wrapper.

--- src/components/expense-item-loading.js
+++ src/components/expense-item-loading.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const ExpenseItemLoading = () => (
-  <tr><td colSpan="5" className="loading-item">Loading expenses...</td></tr>
-);
+const ExpenseItemLoading = () => {
+  return [
+    <tr><td colSpan="5" className="loading-item">Loading expenses...</td></tr>,
+    <tr><td colSpan="5" className="loading-item">Loading expenses...</td></tr>,
+    <tr><td colSpan="5" className="loading-item">Loading expenses...</td></tr>
+  ]
+};
 
 export default ExpenseItemLoading;
